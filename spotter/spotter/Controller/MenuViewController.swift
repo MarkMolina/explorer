f3377928989f1169959e068277a84e6e8b653c7d
Mark Molina <prive@markmolina.nl>
2020-04-23 18:52:47 +0200

    Implemented tap to open menu

---
+++ spotter/spotter/Controller/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  spotter
+//
+//  Created by Mark Anthony Molina on 23/04/2020.
+//  Copyright © 2020 dakke dak. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        view.backgroundColor = UIColor.red
+    }
+}
